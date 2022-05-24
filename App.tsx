d2dd5a26d9e57dd7bb23e3a69420053ab4ea660c
William Candillon <wcandillon@gmail.com>
2022-05-24 16:58:43 +0200

    🧭 Add navigation

--- App.tsx
+++ App.tsx
@@ -1,24 +1,26 @@
-import 'expo-dev-client';
+import * as React from 'react';
+import { View, Text } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
-import { View } from 'react-native';
-import {Canvas, Circle, Fill, Group} from "@shopify/react-native-skia";
-
-export default function App() {
-  const width = 256;
-  const height = 256;
-  const r = 215;
+function HomeScreen() {
   return (
-    <Canvas style={{ flex: 1 }}>
-    <Group blendMode="multiply">
-      <Circle cx={r} cy={r} r={r} color="cyan" />
-      <Circle cx={width - r} cy={r} r={r} color="magenta" />
-      <Circle
-        cx={width/2}
-        cy={height - r}
-        r={r}
-        color="yellow"
-      />
-    </Group>
-  </Canvas>
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Home Screen</Text>
+    </View>
   );
 }
+
+const Stack = createNativeStackNavigator();
+
+function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+export default App;
