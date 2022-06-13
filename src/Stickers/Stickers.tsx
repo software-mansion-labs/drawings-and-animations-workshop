41edffd157381e7717ac72912e1fd22ee3959797
Krzysztof Magiera <krzys@swmansion.com>
2022-06-13 11:14:05 +0200

    Fix aspect ratio of selected photo

--- src/Stickers/Stickers.tsx
+++ src/Stickers/Stickers.tsx
@@ -192,8 +192,6 @@ export const Stickers = ({ navigation }) => {
     // No permissions request is necessary for launching the image library
     let result = await ImagePicker.launchImageLibraryAsync({
       mediaTypes: ImagePicker.MediaTypeOptions.All,
-      allowsEditing: true,
-      aspect: [4, 3],
       quality: 1,
     });
 
